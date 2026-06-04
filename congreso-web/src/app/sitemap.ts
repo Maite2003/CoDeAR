70ff581de879f9b72f6ccb35ceb1a621421082ca
Maite Nigro <86933780+Maite2003@users.noreply.github.com>
2026-06-04 09:20:25 -0300

    Update sitemap URL to new domain

--- congreso-web/src/app/sitemap.ts
+++ congreso-web/src/app/sitemap.ts
@@ -3,7 +3,7 @@ import { MetadataRoute } from 'next'
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: 'https://congresodestiladoresargentinos.vercel.app',
+      url: 'https://www.congresodestiladoresargentinos.com.ar',
       lastModified: new Date(),
       changeFrequency: 'weekly',
       priority: 1,
